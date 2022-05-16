52477d71484ec99478ecec6440d92966b2b86412
Jordan Peck <jpeck008@hammer.cs.ucr.edu>
2022-05-15 19:06:53 -0700

    Adding the Game.cpp driver shell

---
+++ Game.cpp
@@ -0,0 +1,19 @@
+#include "Board.hpp"
+#include "Pieces.hpp"
+#include "Tile.hpp"
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+//	Board* b = new Board();
+//	//
+//	//	for (int i = 0; i <= 7; i++) {
+//	//		for (int j = 0; j <= 7; j++) {
+//	//			cout << b->getTile(i, j)->getPiece()->getName() << " ";
+//	//		}
+//	//		cout << endl;
+//	//	}
+//		return 0;
+//		}
